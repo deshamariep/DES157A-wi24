836d3a4c4b140b1258890002668187e0c9c4dab4
Desha Poindextere <dmpoinz@gmail.com>
2024-03-05 18:35:10 -0800

    added update

--- studio3/script.js
+++ studio3/script.js
@@ -12,6 +12,10 @@
         cards: ['images/cloves1.pdf', 'images/cloves2.pdf', 'images/cloves3.pdf', 'images/cloves4.pdf', 'images/cloves5.pdf', 'images/dia1.pdf', 'images/dia2.pdf', 'images/dia3.pdf', 'images/dia4.pdf', 'images/dia5.pdf', 'images/hearts1.pdf', 'images/hearts2.pdf', 'images/hearts3.pdf', 'images/hearts4.pdf', 'images/hearts5.pdf', 'images/spades1.pdf', 'images/spades2.pdf', 'images/spades3.pdf', 'images/spades4.pdf', 'images/spades5.pdf'],
         players: ['player 1', 'player 2'],
         score: [0, 0],
+        q1: ["red", "black"],
+        q2:["higher", "lower"],
+        q3:["inside", "outside"],
+        q4: ["spades", "hearts", "diamonds", "cloves"]
         // roll1: 0,
         // roll2: 0,
         // rollSum: 0,
@@ -21,11 +25,11 @@
     gameStart.addEventListener('click', function(){
         gameControl.innerHTML = '<h2>You`re playing!</h2>';
         gameControl.innerHTML += '<button id="quit">End Game</button>';
-
+    
+        // Adding event listener after adding the button to the DOM
         document.getElementById('quit').addEventListener('click', function(){
             location.reload();
         });
-
         // setUpTurn();
     });
 })();
